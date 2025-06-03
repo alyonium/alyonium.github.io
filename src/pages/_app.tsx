3a64e4745ea484a59abc10f8f17d8c196a1f302f
alyonium <alyonium2.0@gmail.com>
2025-06-03 18:35:48 +0200

    add metadata, code review

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>alyonium</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta
+          name='keywords'
+          content='alyonium, frontend developer, web developer, portfolio, Alena Stovpets'
+        />
+        <meta name='author' content='Alena Stovpets' />
+        <meta
+          name='description'
+          content="I'm Alena, a frontend developer. Nice to meet you 🙂"
+        />
+        <meta property='og:title' content='alyonium' />
+        <meta
+          property='og:description'
+          content="I'm Alena, a frontend developer. Nice to meet you 🙂"
+        />
+        <meta property='og:url' content='https://alyonium.github.io/' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:image'
+          content='https://alyonium.github.io/og-image.png'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
