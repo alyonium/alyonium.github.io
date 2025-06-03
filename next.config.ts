8ab79fbb220bf014cf863adfcb80d0ffb55a5165
alyonium <alyonium2.0@gmail.com>
2025-06-03 19:07:27 +0200

    deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/alyonium.github.io',
+  assetPrefix: '/alyonium.github.io/',
   reactStrictMode: true,
 };
 
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}', // if using pages directory
+    './styles/**/*.{css,scss}',
+  ],
+};
