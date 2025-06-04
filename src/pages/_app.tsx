8d90d82f71ef6b6891aacd68e5981ffdee3fbff0
alyonium <alyonium2.0@gmail.com>
2025-06-04 16:09:31 +0200

    add normalize css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import 'normalize.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
